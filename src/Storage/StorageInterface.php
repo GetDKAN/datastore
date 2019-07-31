afe73baadf24cbb0ec941cee1a440833aeff118f
Dan Feder <dafeder@gmail.com>
2019-07-31 06:12:25 -0500

    Rename storage interface and add move schema functions from tabular interface

---
+++ src/Storage/StorageInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Dkan\Datastore\Storage;
+
+use Contracts\BulkRetriever;
+use Contracts\Countable;
+use Contracts\StorerInterface;
+
+interface StorageInterface extends 
+    StorerInterface, 
+    Countable, 
+    BulkRetriever
+{
+    public function setSchema($schema);
+
+    public function getSchema();
+  
+}
--- src/Storage/TabularInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Dkan\Datastore\Storage;
-
-interface TabularInterface
-{
-  public function setSchema($schema);
-}
